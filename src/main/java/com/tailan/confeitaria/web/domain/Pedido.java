4a70fb016101ea5c1cec0f473c2118c0eb0d7265
tailan <tailansanttos02@gmail.com>
2025-12-08 19:24:29 -0300

    feat: entidade pedido e enum de status criado e definido.

---
+++ src/main/java/com/tailan/confeitaria/web/domain/Pedido.java
@@ -0,0 +1,62 @@
+package com.tailan.confeitaria.web.domain;
+
+import com.tailan.confeitaria.web.domain.enums.Status;
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Table(name = "tb_pedidos")
+@Entity
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private Instant  momentoPedido;
+
+    //No banco salva como Inteiro para melhor velocidade(1,2,3,4,5,6)
+    private Integer status;
+
+    public Pedido(){
+
+    }
+
+    public Pedido(UUID id, Instant momentoPedido, Integer status) {
+        this.id = id;
+        this.momentoPedido = momentoPedido;
+        this.status = status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Instant getMomentoPedido() {
+        return momentoPedido;
+    }
+
+    public void setMomentoPedido(Instant momentoPedido) {
+        this.momentoPedido = momentoPedido;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Status getStatus(){
+        return Status.valueOf(status);
+    }
+
+    public void setStatus(Status status){
+        if (status != null) {
+            this.status = status.getCode();
+        }
+    }
+
+}
