2bd6c6a8b139b68d806332c1b85fd80ce8a890e6
tailan <tailansanttos02@gmail.com>
2025-12-17 11:19:26 -0300

    feat: repository de product criaod

--- src/main/java/com/tailan/confeitaria/web/repository/ProductRepository.java
+++ src/main/java/com/tailan/confeitaria/web/repository/ProductRepository.java
@@ -1,9 +1,18 @@
 package com.tailan.confeitaria.web.repository;
 
+import com.tailan.confeitaria.web.domain.Category;
 import com.tailan.confeitaria.web.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+    Optional<Product> findByName(String name);
+
+
 }
