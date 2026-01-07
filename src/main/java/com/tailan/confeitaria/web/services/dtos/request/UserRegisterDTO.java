4a8c0d020453a96962186991ab720f106e629205
tailan <tailansanttos02@gmail.com>
2026-01-07 17:20:12 -0300

    feat: dtos de request e response da auth criado e com validações

---
+++ src/main/java/com/tailan/confeitaria/web/services/dtos/request/UserRegisterDTO.java
@@ -0,0 +1,18 @@
+package com.tailan.confeitaria.web.services.dtos.request;
+
+import com.tailan.confeitaria.web.domain.Address;
+import com.tailan.confeitaria.web.domain.Order;
+import com.tailan.confeitaria.web.domain.enums.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+public record UserRegisterDTO(@NotBlank(message = "O noome é obrigatório.")String name,
+                              @NotBlank(message = "O e-mail é obrigatório.") @Email(message = "Formato do e-mail inválido.")String email,
+                              @NotBlank(message = "O telefone é obrigatório.") String phone,
+                              @NotBlank(message = "A senha é obrigatória.")String password,
+                              @NotBlank(message = "O cpf é obrigatório.")String cpf,
+                              @NotBlank(message = "O endereço é obrigatório.") List<AddressDTO> addresses,
+                              UserRole role) {
+}
