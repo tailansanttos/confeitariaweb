c845f816b707d2474bd2913c6ba936c537531867
Tailan Santos <tailansanttos02@gmail.com>
2026-03-02 19:00:15 -0300

    put

--- src/main/java/com/tailan/confeitaria/web/controller/CategoryController.java
+++ src/main/java/com/tailan/confeitaria/web/controller/CategoryController.java
@@ -5,15 +5,14 @@ import com.tailan.confeitaria.web.services.dtos.response.ApiResponseDTO;
 import com.tailan.confeitaria.web.services.dtos.request.CategoryDTO;
 import com.tailan.confeitaria.web.services.dtos.response.CategoryResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -33,5 +32,39 @@ public class CategoryController {
         CategoryResponseDTO categoryResponseDTO =  categoryService.createCategory(categoryDTO);
         ApiResponseDTO responseDTO =  new ApiResponseDTO(categoryResponseDTO, HttpStatus.CREATED.value());
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{category}")
+    @Operation(description = "Deve retornar uma categoria pelo nome")
+    @ApiResponse(responseCode = "200", description = "Categoria encontrada com sucesso.")
+    public  ResponseEntity<ApiResponseDTO> getCategory(@PathVariable("categoryId") String category){
+        CategoryResponseDTO response = categoryService.findByName(category);
+        ApiResponseDTO responseDTO =  new ApiResponseDTO(response, HttpStatus.OK.value());
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+
+    @Operation(description = "Deve retornar as categorias paginados")
+    @ApiResponse(responseCode = "200", description = "Categorias retornadas com sucesso.")
+    @GetMapping
+    public ResponseEntity<ApiResponseDTO> findAllCategories(@RequestParam("page")  int page, @RequestParam("size") int size){
+        Page<CategoryResponseDTO> categories = categoryService.getAllCategories(page, size);
+        ApiResponseDTO responseDTO =  new ApiResponseDTO(categories, HttpStatus.OK.value());
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<ApiResponseDTO>updateCategory(@PathVariable("categoryId")Long categoryId,@Valid @RequestBody CategoryDTO categoryDTO){
+        CategoryResponseDTO categoryResponseDTO =  categoryService.updateCategory(categoryId, categoryDTO);
+        ApiResponseDTO responseDTO =  new ApiResponseDTO(categoryResponseDTO, HttpStatus.OK.value());
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{category}")
+    public ResponseEntity<ApiResponseDTO> deleteCategory(@PathVariable("category") String category){
+        categoryService.deleteCategoyByName(category);
+        ApiResponseDTO responseDTO =  new ApiResponseDTO(HttpStatus.NO_CONTENT.value(), HttpStatus.OK.value());
+        return new ResponseEntity<>(responseDTO, HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/tailan/confeitaria/web/services/CategoryService.java
+++ src/main/java/com/tailan/confeitaria/web/services/CategoryService.java
@@ -3,14 +3,18 @@ package com.tailan.confeitaria.web.services;
 import com.tailan.confeitaria.web.domain.Category;
 import com.tailan.confeitaria.web.services.dtos.request.CategoryDTO;
 import com.tailan.confeitaria.web.services.dtos.response.CategoryResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CategoryService {
     CategoryResponseDTO findByName(String name);
+
     void deleteCategoyByName(String name);
+
     CategoryResponseDTO  createCategory(CategoryDTO category);
     CategoryResponseDTO updateCategory(Long categoryId, CategoryDTO category);
 
-
+    Page<CategoryResponseDTO> getAllCategories(int page, int size);
 
      Category getCategory(String name);
 }
